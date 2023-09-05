39b6bc256f07f02cb2bac011054d08bef64e8237
GusDamPer <gusdamper@gmail.com>
2023-09-05 13:44:34 -0300

    estilos

--- src/app/componentes/home-edit/home-edit.component.ts
+++ src/app/componentes/home-edit/home-edit.component.ts
@@ -19,36 +19,33 @@ export class HomeEditComponent implements OnInit {
  
 
   constructor(
- public homeService: HomeService,
- public formBuilder: FormBuilder,
- private activateRoute: ActivatedRoute,
- private router: Router
+      public homeService: HomeService,
+      public formBuilder: FormBuilder,
+      private activateRoute: ActivatedRoute,
+      private router: Router
+    )
+      { 
+        this.editForm = this.formBuilder.group({
+          imghome : [''],
+          textohome : ['']
+        })
+      }
 
-  )
-
-  { 
-    this.editForm = this.formBuilder.group({
-      imghome : [''],
-      textohome : ['']
-      
+  ngOnInit(): void {
+    const id = this.activateRoute.snapshot.paramMap.get('id');
+    this.homeService.getHomeById(id).subscribe( res => {
+      this.editRef = res;
+      this.editForm = this.formBuilder.group({
+        imghome: [this.editRef.imghome],
+        textohome: [this.editRef.textohome]     
+      }) 
     })
+  }
 
-}
-ngOnInit(): void {
-  const id = this.activateRoute.snapshot.paramMap.get('id');
-  this.homeService.getHomeById(id).subscribe( res => {
-    this.editRef = res;
-    this.editForm = this.formBuilder.group({
-      imghome: [this.editRef.imghome],
-      textohome: [this.editRef.textohome]     
-    }) 
-  })
-}
-
-onSubmit(){
-  const id = this.activateRoute.snapshot.paramMap.get('id');
-  this.homeService.updateDatos(this.editForm.value, id);
-  this.router.navigate(['']);
-}
+  onSubmit(){
+    const id = this.activateRoute.snapshot.paramMap.get('id');
+    this.homeService.updateDatos(this.editForm.value, id);
+    this.router.navigate(['']);
+  }
 
 }
